c68767269fe5bd16f80b855f667e7524058e3a79
Guillaume Bort <guillaume.bort@gmail.com>
2012-06-27 12:22:39 +0200

    README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Cagette
+# Cagette, the quick and dirty data store for prototyping
 
-## The quick and dirty data store for prototyping
+## Setting up with sbt
+
+Configure a new resolver:
+
+```scala
+resolvers += "Guillaume Bort" at "http://guillaume.bort.fr/repository"
+```
+
+Add the library dependency:
+
+```scala
+libraryDependencies += "guillaume.bort" %% "cagette" % "0.1"
+```
+
+
--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ object CagetteBuild extends Build {
 
 	val main = Project(id = "cagette", base = file(".")).settings(
 		version := "0.2-SNAPSHOT",
-		organization := "guillaumebort",
-		publishTo := Some(Resolver.file("file", file("../repository")))
+		organization := "guillaume.bort",
+		publishTo := Some(Resolver.file("file", file(Option(System.getProperty("repository.path")).getOrElse(sys.error("Please set -Drepository.path")))))
 	)
 
 }
