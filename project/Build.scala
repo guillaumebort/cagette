a5e508949da0c5f0af53ca9b7a9da8c66d8c819c
Guillaume Bort <guillaume.bort@gmail.com>
2012-06-27 16:31:54 +0200

    Don't warn for -Drepository.path anymore, but use /tmp to publish instead.

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object CagetteBuild extends Build {
 	val main = Project(id = "cagette", base = file(".")).settings(
 		version := "0.2-SNAPSHOT",
 		organization := "guillaume.bort",
-		publishTo := Some(Resolver.file("file", file(Option(System.getProperty("repository.path")).getOrElse(sys.error("Please set -Drepository.path")))))
+		publishTo := Some(Resolver.file("file", file(Option(System.getProperty("repository.path")).getOrElse("/tmp"))))
 	)
 
 }
